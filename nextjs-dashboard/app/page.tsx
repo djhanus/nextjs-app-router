3628c3f9e0355c6af58fb5387aa4908bf7b95890
djhanus <davidjhanus@gmail.com>
2025-08-15 13:13:14 -0400

    skeleton ui

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -41,10 +41,10 @@ export default function Page() {
           </p>
           <div className={styles.shape} />
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
 
